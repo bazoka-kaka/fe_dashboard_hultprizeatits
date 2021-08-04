bfb1a0b80353ea8d6cf504a0e704cb243712f1bb
bazoka-kaka <55269870+bazoka-kaka@users.noreply.github.com>
2021-08-04 23:12:13 +0700

    feat: update page

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,9 +12,9 @@ export default function Home() {
       </Head>
       <main className='overflow-hidden'>
         <section id='home' className={`relative ${styles.firstSection}`}>
-          <article className={`flex justify-center px-6 md:px-24 flex-col items-left gap-8 text-white py-6 md:py-12 md:gap-12 ${styles.minScreenHeader} layout`}>
+          <article className={`flex justify-center px-10 md:px-24 flex-col items-left gap-8 text-white py-6 md:py-12 md:gap-12 ${styles.minScreenHeader} layout`}>
             <div>
-              <img src='/logo-1.png' className='w-64 lg:w-80 h-auto' />
+              <img src='/logo-1.png' className='w-56 sm:w-56 md:w-64 lg:w-80 h-auto' />
               <h1 className='text-3xl md:text-4xl lg:text-5xl mb-5 mt-3 md:mt-6 font-bold text-left'>WIN THE MOST PRESTIGIOUS<br />BUSINESS IDEA COMPETITION!</h1>
               <p className={`text-lg md:text-xl ${styles.firstP}`}>For ITS students of <span className='font-bold'>ALL</span> study programs & batches</p>
             </div>
@@ -33,16 +33,16 @@ export default function Home() {
           <div className='relative'>
             <img className='w-52 -z-1 h-auto mt-5 absolute' src='/ellipse2.png' alt='' />
           </div>
-          <article className='relative flex justify-center px-4 lg:px-28 flex-col items-left gap-8 text-white py-14 md:gap-12 min-h-screen layout'>
+          <article className='relative flex justify-center px-10 lg:px-28 flex-col items-left gap-8 text-white py-14 md:gap-12 min-h-screen layout'>
             <img className='w-64 h-auto right-0 -bottom-32 absolute' src='/ellipse1.png' alt='' />
             <div className='flex flex-col lg:flex-row'>
               <div className='lg:w-1/2'>
                 <div>
-                  <h1 className='text-2xl md:text-3xl mb-8 text-left'>THE 2022 CHALLENGE<br /><span className='font-bold text-4xl md:text-5xl'>LOREM IPSUM D</span></h1>
+                  <h1 className='text-xl md:text-4xl mb-8 text-left'>THE 2022 CHALLENGE<br /><span className='font-bold text-3xl md:truncate md:text-6xl'>LOREM IPSUM D</span></h1>
                   <p className='text-lg md:text-xl leading-6'>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
                 </div>
-                <div className='z-10 mt-6 self-center lg:self-start'>
-                  <UnstyledLink href='#' className={`text-lg md:text-xl ${styles.thirdBtn}`}>Join the Community!</UnstyledLink>
+                <div className='z-10 mt-10 self-center lg:self-start'>
+                  <UnstyledLink href='#' className={`text-lg rounded-lg px-7 py-2 md:text-xl ${styles.thirdBtn}`}>Join the Community!</UnstyledLink>
                 </div>
               </div>
               <div className='top-10 lg:pl-5 md:w-1/2 self-center relative'>
@@ -53,30 +53,45 @@ export default function Home() {
             </div>
           </article>
         </section>
-        <section id='timeline' className={styles.thirdSection}>
-          <article className='flex flex-col relative pl-28 justify-center items-left gap-2 text-white md:gap-24 min-h-screen layout'>
-              <h1 className='top-8 absolute font-bold text-4xl text-left'>PROGRAM<br />TIMELINE</h1>
-              <div className='flex relative w-full'>
-                <div className={styles.box1}></div>
-                <div className={styles.box2}></div>
-                <div className={styles.box2}></div>
-                <div className={styles.box2}></div>
-                <div className={styles.box2}></div>
+        <section id='timeline' className={`relative ${styles.thirdSection}`}>
+          <article className='flex relative flex-col pl-10 md:pl-28 justify-center items-left gap-2 text-white md:gap-24 min-h-screen layout'>
+              <h1 className='top-8 absolute font-bold text-3xl text-left'>PROGRAM<br />TIMELINE</h1>
+              <div className='flex relative' style={{width: '1300px'}}>
+                <div className={`relative w-80 ${styles.box1}`}>
+                  <h1 className='relative text-black left-5 top-3 font-bold text-2xl'>00 - 00<br/>September</h1>
+                  <h1 className='absolute bottom-3 text-md right-5 text-right text-black font-semibold'>Preliminary<br/><span className='text-2xl font-bold'>Abstract Submission</span></h1>
+                </div>
+                <div className={`relative w-80 ${styles.box2}`}>
+                  <h1 className='relative left-5 top-3 font-semibold text-2xl'>00 - 00<br/>October</h1>
+                  <h1 className='absolute bottom-3 text-md right-5 text-right font-semibold'>Preliminary<br/><span className='text-2xl font-bold'>Abstract Submission</span></h1>
+                </div>
+                <div className={`relative w-80 ${styles.box2}`}>
+                  <h1 className='relative left-5 top-3 font-semibold text-2xl'>00 - 00<br/>November</h1>
+                  <h1 className='absolute bottom-3 text-md right-5 text-right font-semibold'>Preliminary<br/><span className='text-2xl font-bold'>Abstract Submission</span></h1>
+                </div>
+                <div className={`relative w-80 ${styles.box2}`}>
+                  <h1 className='relative left-5 top-3 font-semibold text-2xl'>00 - 00<br/>December</h1>
+                  <h1 className='absolute bottom-3 text-md right-5 text-right font-semibold'>Preliminary<br/><span className='text-2xl font-bold'>Abstract Submission</span></h1>
+                </div>
+                <div className={`relative w-80 ${styles.box2}`}>
+                  <h1 className='relative left-5 top-3 font-semibold text-2xl'>00 - 00<br/>January</h1>
+                  <h1 className='absolute bottom-3 text-lg right-5 text-right font-semibold'>Preliminary<br/><span className='text-2xl font-bold'>Abstract Submission</span></h1>
+                </div>
                 <div className={styles.horizontal}></div>
               </div>
           </article>
-          <UnstyledLink href='#' className={styles.fourthBtn}>Competition Guideline</UnstyledLink>
+          <UnstyledLink href='#' className={`absolute bottom-10 left-10 md:left-28 rounded-lg px-5 py-1 ${styles.fourthBtn}`}>Competition Guideline</UnstyledLink>
         </section>
         <section id='register' className={styles.fourthSection}>
           <article className='relative flex lg:flex-row justify-between px-0 flex-col lg:items-center text-black lg:h-screen min-h-screen layout'>
-            <div className='py-20 lg:py-0 px-4 sm:px-24 lg:px-0 m-auto lg:w-1/2'>
+            <div className='py-20 lg:py-0 px-10 sm:px-24 lg:px-5 m-auto w-full lg:w-1/2'>
                 <div>
                   <h1 className='text-lg text-left'>Elevate your experience by joining the</h1>
                   <img className='mt-2 -ml-1 w-80 h-auto' src='/impact.png' alt='' />
-                  <p className='text-lg w-80 leading-6'>A platform for ITS students who are highly motivated in making an impact.</p>
+                  <p className='text-lg w-50 lg:w-80 leading-6'>A platform for ITS students who are highly motivated in making an impact.</p>
                 </div>
-                <div className='flex mt-7 gap-10'>
-                  <UnstyledLink href='#' className={styles.fifthBtn}>Join the Community!</UnstyledLink>
+                <div className='flex align-center mt-7 w-full'>
+                  <UnstyledLink href='#' className={`${styles.fifthBtn} rounded-lg px-5 py-2`}>Join the Community!</UnstyledLink>
                 </div>
             </div>
             <div className='h-auto lg:h-full w-full lg:w-auto'>
@@ -90,7 +105,7 @@ export default function Home() {
               backgroundImage: 'url(/building-bg.png)',
               backgroundRepeat: 'no-repeat',
             }}
-            className='bg-cover bg-center relative flex px-0 flex-col justify-center items-left gap-8 text-white md:gap-12 min-h-screen layout'
+            className='bg-cover bg-center relative flex px-0 flex-col justify-center items-left gap-8 text-white md:gap-12 py-10 sm:py-0 min-h-screen layout'
           >
             <div className='flex relative'>
               <div className='px-4 md:px-28'>
@@ -105,7 +120,7 @@ export default function Home() {
                 <div className='mt-7 gap-10'>
                   <p className='mb-5 text-lg'>Spread the word about Hult Prize at ITS!</p>
                   <div className='flex flex-col md:flex-row gap-6'>
-                    <UnstyledLink href='#' className={`self-start ${styles.fifthBtn}`}>Download the video</UnstyledLink>
+                    <UnstyledLink href='#' className={`self-start rounded-lg px-5 py-1 ${styles.fifthBtn}`}>Download the video</UnstyledLink>
                     <p className='md:mt-3'>..and share on your Instagram story!</p>
                   </div>
                 </div>
@@ -114,12 +129,12 @@ export default function Home() {
             </div>
           </article>
         </section>
-        <div className='bg-white flex flex-col h-64 w-full'>
+        <div className='flex flex-col h-64 w-full' style={{backgroundColor: '#E5E5E5',}}>
           <div className={styles.footer1}>
-            <h1 className='text-5xl text-white font-bold text-center'>ARE YOU READY TO #CreateTheChange?</h1>
+            <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white font-bold text-center'>ARE YOU READY TO #CreateTheChange?</h1>
           </div>
           <div className={styles.footer2}>
-            <h1 className='text-4xl font-bold text-center'>Stay Connected!</h1>
+            <h1 className='text-3xl md:text-4xl font-bold text-center'>Stay Connected!</h1>
             <div className={styles.connect}>
               <UnstyledLink href='#' ><img src='/instagram.png'/></UnstyledLink>
               <UnstyledLink href='#' ><img src='/linkedin.png'/></UnstyledLink>
