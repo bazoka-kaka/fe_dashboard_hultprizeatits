7cc4ec9878e4160eb38eeaf131ec067a5337a85e
bazoka-kaka <55269870+bazoka-kaka@users.noreply.github.com>
2021-08-17 12:45:08 +0700

    feat: change timeline responsiveness

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -10,10 +10,38 @@ function ImageSlider() {
         time: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '-8',
         cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 1196,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerPadding: '100'
+                }
+            },
+            {
+                breakpoint: 870,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerPadding: '50'
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerPadding: '-8'
+                }
+            }
+        ]
     }
     return (
-        <Slider className='z-10 px-3' {...settings}>
+        <Slider className='z-50 px-3' {...settings}>
             <div className="z-20 card-container">
                 <div className='flex flex-col justify-between px-4 py-4 rounded-lg' style={{width: '250px', height: '370px', background: 'linear-gradient(30.41deg, #C11873 0%, #EC83BD 100%)'}}>
                     <h1 className='text-3xl font-semibold'>OO - OO<br />September</h1>
@@ -33,14 +61,6 @@ function ImageSlider() {
                     <div className='flex flex-col justify-between w-full h-full px-4 py-4 rounded-lg' style={{background: 'black'}}>
                         <h1 className='text-3xl font-semibold text-white'>OO - OO<br />November</h1>
                         <h1 className='text-lg font-semibold text-right text-white'>Road to Final<br/><span className='text-3xl'>Incubation Program</span></h1>
-                    </div>
-                </div>
-            </div>
-            <div className="card-container">
-                <div className='px-1 py-1 rounded-lg' style={{width: '250px', height: '370px', background: 'linear-gradient(30.41deg, #C11873 0%, #EC83BD 100%)'}}>
-                    <div className='flex flex-col justify-between w-full h-full px-4 py-4 rounded-lg' style={{background: 'black'}}>
-                        <h1 className='text-3xl font-semibold text-white'>4 December</h1>
-                        <h1 className='text-lg font-semibold text-right text-white'>Final<br/><span className='text-3xl'>Award<br/>Ceremony<br />&Gala</span></h1>
                     </div>
                 </div>
             </div>
